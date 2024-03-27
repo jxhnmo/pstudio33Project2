0d749703abbf4b965fdac8ef60b0c0146550ebef
John Mo <peikaimo@gmail.com>
2024-03-27 18:57:10 -0500

    working login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,15 @@
 import Image from "next/image";
 import styles from "@/app/login/login.module.css";
+import Link from 'next/link'
+
+
 
 export default function Home() {
   return (
-   <p className = {styles.p}>This is login page</p>
+    <div className="container">
+      <Link className={styles.square} href="/order"> Customer Order</Link>
+      <Link className={styles.square} href="/menu"> Menu</Link>
+      <Link className={styles.square} href="/staff"> Staff</Link>
+    </div>
   );
 }
