6b8fd14da5fcb95b0cfb19b985f3e7460a4cc535
John Mo <peikaimo@gmail.com>
2024-03-27 12:29:23 -0500

    test image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
           >
             By{" "}
             <Image
-              src="/nextjs-github-pages/vercel.svg"
+              src="../public/vercel.svg"
               alt="Vercel Logo"
               className={styles.vercelLogo}
               width={100}
