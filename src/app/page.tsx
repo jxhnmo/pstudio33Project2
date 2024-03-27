a177ddb8f5e185c6e7cc15f0d9bc917c0078c4cb
John Mo <peikaimo@gmail.com>
2024-03-27 12:32:15 -0500

    image test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
       <div className={styles.center}>
         <Image
           className={styles.logo}
-          src="/nextjs-github-pages/next.svg"
+          src="/public/next.svg"
           alt="Next.js Logo"
           width={180}
           height={37}
