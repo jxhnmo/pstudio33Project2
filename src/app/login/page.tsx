6e77714efa1a93e1efaefca62ea7b1e233279ef9
John Mo <peikaimo@gmail.com>
2024-03-27 20:45:31 -0500

    login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,18 +6,23 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className="container">
-      <Link className={styles.square} href="/order"> Customer Order</Link>
-      <Link className={styles.square} href="/menu"> Menu</Link>
-      <div className={styles.square}>
-        <form>
-          <h1>Staff</h1>
-          <label htmlFor="username">Username:</label><br />
-          <input type="text" id="username" name="username" /> <br />
-          <label htmlFor="password">Password:</label><br />
-          <input type="password" id="password" name="password" /> <br />
-          <button type="submit">Login</button>
-        </form>
+    <div>
+      <div>
+        <h1 className={styles.h1}>Welcome to REVS</h1>
+      </div>
+      <div className="container">
+        <Link className={styles.square} href="/order"> Customer Order</Link>
+        <Link className={styles.square} href="/menu"> Menu</Link>
+        <div className={styles.square}>
+          <form>
+            <h1>Staff</h1>
+            <label htmlFor="username">Username:</label><br />
+            <input type="text" id="username" name="username" /> <br />
+            <label htmlFor="password">Password:</label><br />
+            <input type="password" id="password" name="password" /> <br />
+            <button type="submit">Login</button>
+          </form>
+        </div>
       </div>
     </div>
   );
