6866bb6f48d60dbab40d7129e8538e064f12d1d4
Grant Oxford <grantwoxford@gmail.com>
2024-03-28 16:04:16 -0500

    Menu page navigates back to login page

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,8 +1,31 @@
-import Image from "next/image";
+"use client";
+
+import Link from "next/link";
 import styles from "@/app/menu/menu.module.css";
+
+// Just for testing, data should be retrieved from database
+const menuData = [
+  {
+    category: "Appetizers",
+    items: [
+      { name: "Spring Rolls", price: 5.99, imageUrl: "/images/spring-rolls.jpg" },
+      { name: "Garlic Bread", price: 4.99, imageUrl: "/images/garlic-bread.jpg" },
+    ],
+  },
+  {
+    category: "Mains",
+    items: [
+      { name: "Steak", price: 19.99, imageUrl: "/images/steak.jpg" },
+      { name: "Salmon", price: 17.99, imageUrl: "/images/salmon.jpg" },
+    ],
+  },
+];
 
 export default function Home() {
   return (
-   <p className={styles.p}>This is menu page</p>
+    <Link href="/" style={{ width: '100%', height: '100%' }}>
+      <h1>This is the menu</h1>
+    </Link>
   );
 }
+
