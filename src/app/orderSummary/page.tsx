916e6d7cab58a5099106aa86e94b7b80b7b62ca8
thomasbolf <thomasbolf1@tamu.edu>
2024-04-01 15:58:00 -0500

    changed css, made prettier

--- src/app/orderSummary/page.tsx
+++ src/app/orderSummary/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useEffect, useState } from 'react';
-import styles from '@/app/order/order.module.css'; // Adjust the path as necessary
+import styles from '@/app/orderSummary/orderSummary.module.css'; // Adjust the path as necessary
+import { completeTransaction } from '../order';
 
 // If you're placing this in the same file
 interface Item {
@@ -22,9 +23,17 @@ const OrderSummary = () => {
   // Calculate the total price
   const totalPrice = selectedItems.reduce((acc, item) => acc + item.price * item.quantity, 0);
 
+  const handleConfirmOrder = () => {
+    const currentTime: Date = new Date();
+    // console.log(currentTime.toISOString());
+    completeTransaction(totalPrice.toFixed(2), selectedItems);
+    // selectedItems.map((item,index) => {console.log(item.id+item.name+item.price+item.quantity)})
+    setSelectedItems([]);
+  };
+
   return (
     <div className={styles.main}>
-      <h1>Order Summary</h1>
+      <h1 className={styles.orderSummaryHeader}>Order Summary</h1>
       <div className={styles.orderList}>
         {selectedItems.length > 0 ? (
           selectedItems.map((item, index) => (
@@ -39,6 +48,9 @@ const OrderSummary = () => {
       <div className={styles.total}>
         Total: <span>${totalPrice.toFixed(2)}</span>
       </div>
+      <button onClick={handleConfirmOrder} className={styles.confirmOrderButton}>
+          Confirm Order
+        </button>
     </div>
   );
 };
