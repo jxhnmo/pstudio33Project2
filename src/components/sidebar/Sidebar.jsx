32a9c3a78392215673e584c34a8c7c8a00b69075
Grant Oxford <grantwoxford@gmail.com>
2024-04-02 20:08:20 -0500

    Styled setting panel better

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import styles from './sidebar.module.css';
 
 /** SetTheme function for toggling between high-contrast and default themes */
@@ -15,7 +17,7 @@ const SetTheme = () => {
     global.window.__onThemeChange = setTheme;
   }, []);
 
-  return <button onClick={toggleTheme}>{isHighContrast ? 'default' : 'high-contrast'}</button>;
+  return <button onClick={toggleTheme} className={styles.settingButton}>{isHighContrast ? 'ON' : 'OFF'}</button>;
 };
 
 /** Siddebar function for handling opening and closing the sidebar */
@@ -27,10 +29,13 @@ const Sidebar = () => {
   return (
     <div className={isOpen ? `${styles.sidebar} ${styles.open}` : styles.sidebar}>
       <button onClick={toggleSidebar} className={styles.toggleButton}>
-        {isOpen ? 'Close' : 'Open'}
+        <FontAwesomeIcon icon={isOpen ? faChevronLeft : faChevronRight} />
       </button>
       {
-        <SetTheme />
+        <>
+          <h3 className={styles.settingName}>High Contrast Mode</h3>
+          <SetTheme />
+        </>
       }
     </div>
   );
