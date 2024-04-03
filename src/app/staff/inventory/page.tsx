1f6e75e4f49af8c5199cea5935be66e09f8554fd
John Mo <peikaimo@gmail.com>
2024-04-02 20:16:09 -0500

    update staff order page

--- src/app/staff/inventory/page.tsx
+++ src/app/staff/inventory/page.tsx
@@ -3,9 +3,12 @@ import styles from "@/app/staff/inventory/staffInventory.module.css";
 
 export default function StaffInventory() {
   return (
-    <div>
-      <p className={styles.p}>This is the Staff Inventory page.</p>
-      
+    <div className={styles.main}>
+      <div className={styles.container}>
+        <p className={styles.p}>This is the Staff Inventory page.</p>
+      </div>
+
+
       {/* Navigation Buttons */}
       <div className={styles.buttonsContainer}>
         <Link href="/staff/order" legacyBehavior>
