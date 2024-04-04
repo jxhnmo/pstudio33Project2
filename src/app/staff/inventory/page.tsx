4d1e038b77977547c95edd7ccd8318860636085c
John Mo <peikaimo@gmail.com>
2024-04-03 21:52:27 -0500

    inventory page displayed

--- src/app/staff/inventory/page.tsx
+++ src/app/staff/inventory/page.tsx
@@ -1,17 +1,77 @@
+"use client";
+
 import Link from 'next/link';
 import styles from "@/app/staff/inventory/staffInventory.module.css";
 import dynamic from 'next/dynamic';
+
+import { useEffect, useState } from 'react';
+
+import { fetchInventory } from '../../inventory';
+
+
 const Sidebar = dynamic(() => import('../../../components/sidebar/Sidebar'), {
   ssr: false,
 });
 
+interface Item {
+  id: number;
+  item_name: string;
+  max_stock: number;
+  price: number;
+  stock: number;
+}
+
+
 export default function StaffInventory() {
+  const [inventory, setInventory] = useState([]);
+
+
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        const data = await fetchInventory();
+        console.log(data);
+        setInventory(data);
+      }
+      catch (error) {
+        console.error("Failed to fetch:", error);
+      }
+    }
+
+    loadData();
+  }, []);
+
   return (
     <>
       <Sidebar />
+
       <div className={styles.main}>
+
+        <h1 className={styles.title}>Inventory</h1>
         <div className={styles.container}>
-          <p className={styles.p}>This is the Staff Inventory page.</p>
+          {/* Inventory Grid */}
+          <table className={styles.inventoryTable}>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Item Name</th>
+                <th>Max Stock</th>
+                <th>Price</th>
+                <th>Current Stock</th>
+              </tr>
+            </thead>
+            <tbody>
+              {inventory.map((item: Item) => (
+                <tr key={item.id}>
+                  <td>{item.id}</td>
+                  <td>{item.item_name}</td>
+                  <td>{item.max_stock}</td>
+                  <td>${item.price}</td>
+                  <td>{item.stock}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
 
 
