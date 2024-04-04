f72e9eab1395be591a21761b793ad62529b4fe07
John Mo <peikaimo@gmail.com>
2024-04-04 12:15:32 -0500

    ordering view font size update

--- src/app/staff/order/page.tsx
+++ src/app/staff/order/page.tsx
@@ -94,57 +94,60 @@ export default function Home() {
 
   return (
     <>
-     <Sidebar />
-      <div className={styles.main}>
-        <div className={styles.orderContainer}>
-          {/* Categories Column */}
-          <div className={styles.categories}>
-            <h2 className={styles.categoriesHeader} onClick={handleReturnHome}>Categories</h2>
-            <div className={styles.categoriesList}>
-              {categories.map((categoryName, index) => (
-                <button
-                  key={index}
-                  className={`${styles.categoryButton} ${activeCategory === categoryName ? styles.activeCategory : ''}`}
-                  onClick={() => loadItemsForCategory(categoryName)}
-                >
-                  {categoryName}
-                </button>
-              ))}
-            </div>
-          </div>
-          {/* Order Menu */}
-          <div className={styles.orderMenu}>
-            {currentCategoryItems.map((item, index) => (
-              <button key={index} onClick={() => handleSelectItem(item)}>
-                {<Image src={`/images/${item.name.replace(/\s/g, '')}.png`} alt={item.name} width={100} height={100} />}
-                {item.name}
-              </button> // Adjust to match your item object structure
-            ))}
-          </div>
-          {/* Current Order Column */}
-          <div className={styles.currentOrder}>
-            <div className={styles.currOrderTop}>
-              <h2 className={styles.currentOrderTitle}>Current Order</h2>
-              <div className={styles.orderList}>
-                {selectedItems.map((item: { name: string, price: number, quantity: number }, index: number) => (
-                  <div key={index}>
-                    {item.name} - ${item.price} x {item.quantity}
-                  </div>
+      <div className={styles.pageContainer}>
+        <Sidebar />
+        <div className={styles.main}>
+          <div className={styles.orderContainer}>
+            {/* Categories Column */}
+            <div className={styles.categories}>
+              <h2 className={styles.categoriesHeader} onClick={handleReturnHome}>Categories</h2>
+              <div className={styles.categoriesList}>
+                {categories.map((categoryName, index) => (
+                  <button
+                    key={index}
+                    className={`${styles.categoryButton} ${activeCategory === categoryName ? styles.activeCategory : ''}`}
+                    onClick={() => loadItemsForCategory(categoryName)}
+                  >
+                    {categoryName}
+                  </button>
                 ))}
               </div>
             </div>
-            <div className={styles.currOrderBtm}>
-              <div className={styles.total}>
-                Total: <span>${totalPrice.toFixed(2)}</span>
+            {/* Order Menu */}
+            <div className={styles.orderMenu}>
+              {currentCategoryItems.map((item, index) => (
+                <button key={index} onClick={() => handleSelectItem(item)}>
+                  {<Image src={`/images/${item.name.replace(/\s/g, '')}.png`} alt={item.name} width={100} height={100} />}
+                  {item.name}
+                </button> // Adjust to match your item object structure
+              ))}
+            </div>
+            {/* Current Order Column */}
+            <div className={styles.currentOrder}>
+              <div className={styles.currOrderTop}>
+                <h2 className={styles.currentOrderTitle}>Current Order</h2>
+                <div className={styles.orderList}>
+                  {selectedItems.map((item: { name: string, price: number, quantity: number }, index: number) => (
+                    <div key={index}>
+                      {item.name} - ${item.price} x {item.quantity}
+                    </div>
+                  ))}
+                </div>
               </div>
-              <button onClick={handleConfirmOrder} className={styles.confirmOrderButton}>
-                Confirm Order
-              </button>
+              <div className={styles.currOrderBtm}>
+                <div className={styles.total}>
+                  Total: <span>${totalPrice.toFixed(2)}</span>
+                </div>
+                <button onClick={handleConfirmOrder} className={styles.confirmOrderButton}>
+                  Confirm Order
+                </button>
+              </div>
             </div>
           </div>
         </div>
+
         {/* Navigation Buttons */}
-        <div className={styles.footer}>
+        <div className={styles.buttonsContainer}>
           <Link href="/staff/order" legacyBehavior>
             <a className={styles.navButton}>Staff Order</a>
           </Link>
@@ -157,5 +160,6 @@ export default function Home() {
         </div>
       </div>
     </>
+
   );
 }
