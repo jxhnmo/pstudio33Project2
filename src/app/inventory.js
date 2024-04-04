1e1123fc8c0c9ad8d8d68d5888e834d84619b0ca
John Mo <peikaimo@gmail.com>
2024-04-03 22:48:53 -0500

    inventory can add new inventory item

--- src/app/inventory.js
+++ src/app/inventory.js
@@ -19,3 +19,28 @@ export async function fetchInventory() {
         return [];
     }
 }
+
+export async function addItem(newItem) {
+    const pool = new Pool({
+        user: process.env.DATABASE_USER,
+        host: process.env.DATABASE_HOST,
+        database: process.env.DATABASE_NAME,
+        password: process.env.DATABASE_PASSWORD,
+        port: 5432,
+    });
+
+    try {
+        const idResult = await pool.query('SELECT MAX(id) as max_id FROM inventory_items;');
+        let newId = idResult.rows[0].max_id ? idResult.rows[0].max_id + 1 : 1;
+
+        const insertQuery = 'INSERT INTO inventory_items(id, item_name, max_stock, price, stock) VALUES($1, $2, $3, $4, $5) RETURNING *;';
+        const values = [newId, newItem.item_name, newItem.max_stock, newItem.price, newItem.stock];
+        const insertResult = await pool.query(insertQuery, values);
+
+        await pool.end();
+        return insertResult.rows[0];
+    } catch (err) {
+        console.error('Failed to add item', err);
+        throw err;
+    }
+}
