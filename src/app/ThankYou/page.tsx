41bcff2c8ff6b32f47f1098acdb26664291524d7
thomasbolf <thomasbolf1@tamu.edu>
2024-04-04 13:16:49 -0500

    if you click on the sliding menu, it sends you to the order

--- src/app/ThankYou/page.tsx
+++ src/app/ThankYou/page.tsx
@@ -12,12 +12,12 @@ const ThankYou = () => {
   }, [router]);
 
   const orderId = localStorage.getItem('orderId');
-
+  
   return (
     <div className={styles.container}>
       <h1 className={styles.header}>Thank you for your business!</h1>
       <p className={styles.message}>Your order will be out shortly.</p>
-      {orderId && <p className={styles.message}>Your order, {orderId}, will be out shortly.</p>}
+      {orderId && <p className={styles.message}>Your order, {orderId} , will be out shortly.</p>}
     </div>
   );
 };
