075944322c0c1bad19938107fb2df8cf7fd67ae3
thomasbolf <thomasbolf1@tamu.edu>
2024-04-04 19:12:46 -0500

    fixed problem with nav bar and confirm order button

--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -109,7 +109,7 @@ export default function Home() {
     }
     //completeTransaction(totalPrice.toFixed(2), selectedItems);
 
-    // setSelectedItems([]);
+    //setSelectedItems([]);
   };
 
   const handleReturnHome = () => {
