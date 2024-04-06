af045c07383bfc26070e8e85e0dc76e4a5e14865
John Mo <peikaimo@gmail.com>
2024-04-06 10:48:53 -0500

    setup initial analytics fetch and pull

--- src/app/staff/stats/page.tsx
+++ src/app/staff/stats/page.tsx
@@ -1,17 +1,48 @@
+"use client";
+
 import Link from 'next/link';
 import styles from "@/app/staff/stats/staffStats.module.css";
 import dynamic from 'next/dynamic';
+import { useEffect, useState } from 'react';
+
+import { fetchData } from '../../analytics';
+
 const Sidebar = dynamic(() => import('../../../components/sidebar/Sidebar'), {
   ssr: false,
 });
 
+interface orderData {
+  id: number;
+  cost: number;
+  employee_id: number;
+  purchase_time: string;
+
+}
+
 export default function StaffStats() {
+  const [data, setData] = useState<[]>([]);
+
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        const data = await fetchData();
+        console.log(data);
+        setData(data);
+      }
+      catch (error) {
+        console.error("Failed to fetch", error);
+      }
+    }
+
+    loadData();
+  }, []);
+
   return (
     <>
       <Sidebar />
       <div>
         <p className={styles.p}>This is the Staff Stats page.</p>
-        
+
         {/* Navigation Buttons */}
         <div className={styles.buttonsContainer}>
           <Link href="/staff/order" legacyBehavior>
