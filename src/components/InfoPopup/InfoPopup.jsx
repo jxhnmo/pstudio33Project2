56f34799d656c234c6492f6b3aee23fdf72cc336
thomasbolf <thomasbolf1@tamu.edu>
2024-04-10 14:35:14 -0500

    made ingredients pop up slightly prettier

--- src/components/InfoPopup/InfoPopup.jsx
+++ src/components/InfoPopup/InfoPopup.jsx
@@ -23,14 +23,14 @@ const InfoPopup = ({ isOpen, itemIngredients, itemInfo, onClose }) => {
   return (
     <div className={styles.popupBackground}>
       <div className={styles.popupContent}>
-        <h2>{itemInfo?.name}</h2>
+        <h2 className={styles.title}>{itemInfo?.name}</h2>
         {/* Render ingredients as a list */}
         <div>
-          <h3>Ingredients</h3>
+          <h3 className={styles.subtitle}>Ingredients</h3>
           {renderIngredientsList(itemIngredients)}
         </div>
-        <p>{itemInfo?.description}</p>
-        <button className={styles.closeButton} onClick={onClose} style={{ color: 'black' }}>Close</button>
+        <p className={styles.description}>{itemInfo?.description}</p>
+        <button className={styles.closeButton} onClick={onClose}>Close</button>
       </div>
     </div>
   );
