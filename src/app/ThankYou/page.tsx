37187be482d35f08a8e89eae98549de3c54a1f47
thomasbolf <thomasbolf1@tamu.edu>
2024-04-11 20:43:37 -0500

    if it's clear it's a different style than if its not clear

--- src/app/ThankYou/page.tsx
+++ src/app/ThankYou/page.tsx
@@ -1,13 +1,18 @@
 "use client";
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation'; // Adjusted import for useRouter
 import styles from './thankYou.module.css'; // Adjust the path as necessary
-import { fetchWeather } from '../weather';
+
 const ThankYou = () => {
   const router = useRouter();
-
+  // State to manage style based on weatherData presence
+  const [isWeatherClear, setIsWeatherClear] = useState(false);
 
   useEffect(() => {
+    // Check if 'weatherData' is clear or not and set style state
+    const weatherData = localStorage.getItem('weatherData');
+    setIsWeatherClear(!weatherData);
+
     if(localStorage.getItem('role') === 'staff'){
       const timer = setTimeout(() => router.push('/staff/order'), 5000);
       return () => clearTimeout(timer);
@@ -18,8 +23,11 @@ const ThankYou = () => {
 
   const orderId = localStorage.getItem('orderId');
   
+  // Use different class based on isWeatherClear state
+  const containerStyle = isWeatherClear ? styles.containerClear : styles.containerNotClear;
+  
   return (
-    <div className={styles.container}>
+    <div className={containerStyle}>
       <h1 className={styles.header}>Thank you for your business!</h1>
       <p className={styles.message}>Your order will be out shortly.</p>
       {orderId && <p className={styles.message}>Your order, {orderId} , will be out shortly.</p>}
