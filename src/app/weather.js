7aba055a04a81f147d7ab91fae286ac9364e6a69
thomasbolf <thomasbolf1@tamu.edu>
2024-04-18 12:00:30 -0500

    added comprehensive weather popup

--- src/app/weather.js
+++ src/app/weather.js
@@ -18,7 +18,6 @@ export async function fetchWeather() {
 
 export async function fetchWeatherAllData() {
     const url = `https://api.openweathermap.org/data/2.5/weather?zip=77840,us&appid=${process.env.WEATHER_KEY}`;
-
     try {
         console.log('about to get response');
         const response = await axios.get(url);
@@ -28,3 +27,44 @@ export async function fetchWeatherAllData() {
         return "NO WEATHER DATA";
     }
 }
+/* sample output:
+Weather key is being read in: 
+a85d0102a70cce7af219d15cf0e6b767
+About to get response
+{
+  coord: { lon: -96.3123, lat: 30.6045 },
+  weather: [
+    {
+      id: 804,
+      main: 'Clouds',
+      description: 'overcast clouds',
+      icon: '04d'
+    }
+  ],
+  base: 'stations',
+  main: {
+    temp: 300.42,
+    feels_like: 303.18,
+    temp_min: 299.29,
+    temp_max: 301.54,
+    pressure: 1014,
+    humidity: 77
+  },
+  visibility: 10000,
+  wind: { speed: 6.69, deg: 160 },
+  clouds: { all: 100 },
+  dt: 1713457737,
+  sys: {
+    type: 2,
+    id: 2082309,
+    country: 'US',
+    sunrise: 1713441222,
+    sunset: 1713488088
+  },
+  timezone: -18000,
+  id: 0,
+  name: 'College Station',
+  cod: 200
+}
+
+  */
