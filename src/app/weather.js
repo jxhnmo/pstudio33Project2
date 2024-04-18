834e09fe768293734098654cb07e2f116ae1f9fd
thomasbolf <thomasbolf1@tamu.edu>
2024-04-18 11:39:44 -0500

    added test file, trying to add weather popup

--- src/app/weather.js
+++ src/app/weather.js
@@ -5,10 +5,24 @@ export async function fetchWeather() {
     const url = `https://api.openweathermap.org/data/2.5/weather?zip=77840,us&appid=${process.env.WEATHER_KEY}`;
 
     try {
+
         console.log('about to get response');
         const response = await axios.get(url);
         return response.data.weather[0].main;
+    } catch (error) {
+        console.error("Failed to fetch weather data:", error);
+        return "NO WEATHER DATA";
+    }
+}
 
+
+export async function fetchWeatherAllData() {
+    const url = `https://api.openweathermap.org/data/2.5/weather?zip=77840,us&appid=${process.env.WEATHER_KEY}`;
+
+    try {
+        console.log('about to get response');
+        const response = await axios.get(url);
+        return response.data;
     } catch (error) {
         console.error("Failed to fetch weather data:", error);
         return "NO WEATHER DATA";
