9811361716171639080b2dec668df8d7482fa3c0
evan3660 <etvan13@tamu.edu>
2024-04-24 13:06:30 -0500

    Small db employee adjustments

--- src/app/auth.js
+++ src/app/auth.js
@@ -6,7 +6,7 @@ import { Pool } from 'pg'; // Connect to PostgreSQL
 const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  process.env.GOOGLE_REDIRECT_URL_LOCAL // Adjust based on environment
+  process.env.GOOGLE_REDIRECT_URL_PUB // Adjust based on environment
 );
 
 const pool = new Pool({
